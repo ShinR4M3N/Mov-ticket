ecbbe35487d126463afc3794bbf98ae1c7124245
ShinR4M3N <shini@middleton.school.nz>
2023-03-14 11:10:08 +1300

    Create Movie_tickets v1.py

---
+++ Movie_tickets v1.py
@@ -0,0 +1,21 @@
+'''Movie theatre ticketing system- v1
+welcome screen and set up variables
+Created by Isaac Shin
+'''
+
+#component 1 - welcome screen and set up variables
+
+
+def sell_ticket():
+    print("********** Fanfare Movies - ticketing system **********\n")
+
+    adult_tickets = 0
+    child_tickets = 0
+    student_tickets = 0
+    gift_ticket = 0
+    total_sales = 0
+    tickets_sold = 0
+
+
+# Main routine
+sell_ticket()
